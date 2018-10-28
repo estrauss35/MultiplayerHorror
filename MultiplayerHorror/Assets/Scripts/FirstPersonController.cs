b757bdef099d482b9cfc905ef43bee938f821ca8
estrauss35 <ems394@nau.edu>
2018-10-27 19:37:23 -0700

    Added the beginning of some player networking
    2 players can connect, but controls are not independant.

--- MultiplayerHorror/Assets/Scripts/FirstPersonController.cs
+++ MultiplayerHorror/Assets/Scripts/FirstPersonController.cs
@@ -2,13 +2,14 @@ using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 using UnityStandardAssets.Utility;
+using UnityEngine.Networking;
 using Random = UnityEngine.Random;
 
 namespace UnityStandardAssets.Characters.FirstPerson
 {
     [RequireComponent(typeof (CharacterController))]
     [RequireComponent(typeof (AudioSource))]
-    public class FirstPersonController : MonoBehaviour
+    public class FirstPersonController : NetworkBehaviour
     {
         [SerializeField] private bool m_IsWalking;
         [SerializeField] private float m_WalkSpeed;
@@ -65,6 +66,10 @@ namespace UnityStandardAssets.Characters.FirstPerson
         // Update is called once per frame
         private void Update()
         {
+            if(!isLocalPlayer)
+            {
+                return;
+            }
             RotateView();
             // the jump state needs to read here to make sure it is not missed
             if (!m_Jump)
